d3bb651715e90087e62be445a7453858614d513f
Enkrl <Enkrllastlegion@gmail.com>
2021-10-05 18:42:01 +0300

    tree update

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-import sklearn as sk
+from sklearn.tree import RandomForestRegressor
+from sklearn.metrics import mse_score
+
+from module_a_dev import polynom_3
+from module_b_dev import hyperbola
+
+x = np.random.uniform(0, 1)
+y = polynom_3(x)
+
+model = RandomForestRegressor()
+model.fit(x, y)
+
+prediction = model.predict(x)
+print(mse_score(y, prediction))
